8c9de1182250ec55a8145a9dd8bb5f6ed12ce244
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-12-09 18:04:23 -0800

    scraping changing rotation as of rn

--- Assets/Scripts/RotateClockWise.cs
+++ Assets/Scripts/RotateClockWise.cs
@@ -7,14 +7,32 @@ public class RotatingClockWise: MonoBehaviour
 {
     public GameObject objectToRotate;
     Quaternion targetRotation;
-    public int speed = 1;
+    private  int speed = 1;
+    float timer = 0f;
+    float tolerance = 0.01f; // Define a small tolerance
+
 
     void Update()
     {
     }
     void FixedUpdate()
     {
-        targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y + speed, objectToRotate.transform.eulerAngles.z);
+        timer += Time.deltaTime;
+        // if(Time.time >= 10)
+        // {
+
+           
+
+        // if (Mathf.Abs(Time.time % 5) < tolerance) // Check if close to a multiple of 5
+        // {
+        //     speed = 1;
+        // }
+        // else
+        // {
+        //     speed = -1;
+        // }
+        // }
+        targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y - speed, objectToRotate.transform.eulerAngles.z);
         objectToRotate.transform.rotation = targetRotation;
     }
 }
--- Assets/Scripts/RotateCounterWise.cs
+++ Assets/Scripts/RotateCounterWise.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
-using UnityEditor;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
-public class RotatingCounterWise: MonoBehaviour
+public class RotatingCounter: MonoBehaviour
 {
     public GameObject objectToRotate;
     Quaternion targetRotation;
-    public int speed = 1;
+    private  int speed = 1;
+    float timer = 0f;
+    float tolerance = 0.01f; // Define a small tolerance
+
 
     void Update()
     {
-
     }
     void FixedUpdate()
     {
-        if(Time.deltaTime % 5 == 0)
-        {
-            speed = -1;
-            targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y - speed, objectToRotate.transform.eulerAngles.z);
-            objectToRotate.transform.rotation = targetRotation;
-        }
-        else
-        {
-            speed = 1;
-            targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y - speed, objectToRotate.transform.eulerAngles.z);
-            objectToRotate.transform.rotation = targetRotation;
-        }
+        timer += Time.deltaTime;
+        // if(Time.time >= 10)
+        // {
+
+           
+
+        // if (Mathf.Abs(Time.time % 5) < tolerance) // Check if close to a multiple of 5
+        // {
+        //     speed = 1;
+        // }
+        // else
+        // {
+        //     speed = -1;
+        // }
+        // }
+        targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y - speed, objectToRotate.transform.eulerAngles.z);
+        objectToRotate.transform.rotation = targetRotation;
     }
 }
